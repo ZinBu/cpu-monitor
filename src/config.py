f35485f9ca504a084ed0ebf24b5fda92d27dd184
drs <romadmt92@gmail.com>
2023-10-15 15:05:32 +0300

    Enum integrated

--- src/config.py
+++ src/config.py
@@ -7,28 +7,14 @@ DIGIT_SIZE = 45
 MAP_SIZE = 64, 64
 LINUX_PLATFORM = 'linux'
 
+FONT = 'Arial'
+ICON_BG = 'transparent'
+
 COLOR_SET_MSG = 'Color set: '
 CLOSE_MSG = 'Close'
 
 DB_DIR = '_db'
 DB_NAME = os.path.join(DB_DIR, 'config')
-
-
-COLORS = dict(
-    Auto=None,
-    Red=(255, 0, 0),
-    MediumVioletRed=(199, 21, 133),
-    DeepPink=(255, 20, 147),
-    OrangeRed=(255, 69, 0),
-    DarkOrange=(255, 140, 0),
-    PapayaWhip=(255, 239, 213),
-    MediumOrchid=(186, 85, 211),
-    PaleGreen=(152, 251, 152),
-    Lime=(0, 255, 0),
-    MediumSlateBlue=(123, 104, 238),
-    DeepSkyBlue=(0, 191, 255),
-    SteelBlue=(30, 144, 255)
-)
 
 
 class Colors(Enum):
--- src/gui/tray_counter.py
+++ src/gui/tray_counter.py
@@ -11,15 +11,13 @@ from src import tools
 from src.protocols import Metric, Database, DBConfigData
 
 
-# TODO Translate !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 class TrayCounter(QMainWindow):
     # Widget's slots
     TRAY_ICON_SLOT = QtCore.pyqtSignal(str)
     TRAY_ICON_COLOR_SLOT = QtCore.pyqtSignal(str)
 
-    DEFAULT_COLOR_NAME = 'Auto'
-
-    COLORS = config.COLORS
+    COLORS = config.Colors
+    DEFAULT_COLOR = config.Colors.Auto
 
     def __init__(self, metric: type[Metric], db: Database):
         # It's necessary
@@ -35,17 +33,10 @@ class TrayCounter(QMainWindow):
         self.tray_icon = QSystemTrayIcon(self)
         self.painter = QtGui.QPainter()
 
-        self._last_color_name = self.DEFAULT_COLOR_NAME
-        # Задаем дефолтный цвет
-        self._selected_color_value = self.COLORS[self.DEFAULT_COLOR_NAME]
-        # Задаем дефолтную иконку, если что пойдет не так
+        self._selected_color = self.DEFAULT_COLOR
+        # Set a default icon for an unpredictable situation
         self.default_icon = self.style().standardIcon(QStyle.SP_DesktopIcon)  # type: ignore
-        # Ставим первоначальную иконку
-        self.set_icon_in_tray()
-        # Выводим информационное сообщение, задаем цвет
-        # и производим прочие настройки
         self.set_up()
-        # Запускаем мониторинг
         self.run_monitoring()
 
     @tools.thread
@@ -67,53 +58,54 @@ class TrayCounter(QMainWindow):
             self.tray_icon.setIcon(QIcon(icon))
 
     def set_color(self, color_name: str) -> None:
-        self._selected_color_value = self.COLORS[color_name]
+        previous_color_name = self._selected_color.name
+        self._selected_color = self.COLORS[color_name]
         # Disable chosen color
         getattr(self, color_name).setDisabled(True)
         # Release the previous one
-        getattr(self, self._last_color_name).setDisabled(False)
-        self._last_color_name = color_name
+        getattr(self, previous_color_name).setDisabled(False)
         self._show_message(f'{config.COLOR_SET_MSG} {color_name}', 100)
         self._save_config(color_name)
 
     def draw_digit(self, digit: str) -> QtGui.QPixmap:
-        """Создание иконки и рисование цифры на ней"""
-        # Позиционируем цифру на виджете
+        """Creates the icon and draw digits"""
+        # Set digits position
         digit_place: tuple = 0 if len(digit) > 1 else 20, 50
-        # Создаем холстик для рисования цифры иконку
+        # Creates canvas
         icon = QtGui.QPixmap(*config.MAP_SIZE)
-        icon.fill(QtGui.QColor("transparent"))
-        # Рисуем на холстике текст
+        icon.fill(QtGui.QColor(config.ICON_BG))
+        # Drawing
         self.painter.begin(icon)
         self.painter.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
         self.painter.setPen(QtGui.QColor(*self.get_digits_color(digit)))
-        self.painter.setFont(QtGui.QFont('Arial', config.DIGIT_SIZE))
+        self.painter.setFont(QtGui.QFont(config.FONT, config.DIGIT_SIZE))
         self.painter.drawText(QPointF(*digit_place), digit)
         self.painter.end()
         return icon
 
     def get_digits_color(self, digit: str) -> tuple[int, int, int]:
-        if not self._selected_color_value:
+        if not self._selected_color.value:
             digit = int(digit)
             if digit < 40:
-                return self.COLORS['PaleGreen']
+                return self.COLORS.PaleGreen.value
             elif digit < 55:
-                return self.COLORS['SteelBlue']
+                return self.COLORS.SteelBlue.value
             elif digit < 75:
-                return self.COLORS['DarkOrange']
+                return self.COLORS.DarkOrange.value
             else:
-                return self.COLORS['Red']
-        return self._selected_color_value
+                return self.COLORS.Red.value
+        return self._selected_color.value
 
     def set_up(self) -> None:
+        self.set_icon_in_tray()
         self._load_and_set_color_config()
         tray_menu = QMenu()
         quit_action = QAction(config.CLOSE_MSG, self)
         quit_action.triggered.connect(self._close)
 
         # Create menu
-        for name in self.COLORS:
-            self._set_context_color(tray_menu, name)
+        for color in self.COLORS:
+            self._set_context_color(tray_menu, color)
 
         tray_menu.addAction(quit_action)
         self.tray_icon.setContextMenu(tray_menu)
@@ -124,12 +116,12 @@ class TrayCounter(QMainWindow):
         msg_type = QSystemTrayIcon.Information  # type: ignore
         self.tray_icon.showMessage(self.metric.get_name(), msg, msg_type, time)
 
-    def _set_context_color(self, tray_menu: QMenu, name: str) -> None:
-        color_action = QAction(name, self)
-        if self.COLORS[name] == self._selected_color_value:
+    def _set_context_color(self, tray_menu: QMenu, color: config.Colors) -> None:
+        color_action = QAction(color.name, self)
+        if color == self._selected_color:
             color_action.setDisabled(True)
-        setattr(self, name, color_action)
-        color_action.triggered.connect(lambda: self.TRAY_ICON_COLOR_SLOT.emit(name))
+        setattr(self, color.name, color_action)
+        color_action.triggered.connect(lambda: self.TRAY_ICON_COLOR_SLOT.emit(color.name))
         tray_menu.addAction(color_action)
 
     def _save_config(self, color: str) -> None:
@@ -138,8 +130,7 @@ class TrayCounter(QMainWindow):
     def _load_and_set_color_config(self) -> None:
         conf = self.db.load_config()
         if conf.color:
-            self._selected_color_value = self.COLORS[conf.color]
-            self._last_color_name = conf.color
+            self._selected_color = self.COLORS[conf.color]
 
     def _close(self) -> None:
         self.tray_icon.hide()
