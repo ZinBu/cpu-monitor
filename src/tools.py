429b105e4b9207797f76ec22c8e22941d00ef34c
drs <romadmt92@gmail.com>
2023-10-15 15:17:17 +0300

    impr

--- src/tools.py
+++ src/tools.py
@@ -4,18 +4,17 @@ import threading
 
 
 def thread(func):
-    """Декоратор для запуска функций в потоке без контроля исполнения"""
+    """Decorator for running functions in a thread without execution control"""
     def run(*args, **kwargs):
         target = threading.Thread(target=func, args=args, kwargs=kwargs)
-        # если False, то работает после завершения главного процесса
-        target.setDaemon(True)
+        target.deamon = True
         target.start()
         return target
     return run
 
 
 def executable_file_path(file_path: str) -> str:
-    """Определение пути файла в зависимости от того как исполняется приложение"""
+    """Determining the file path depending on how the application is executed"""
     if getattr(sys, 'frozen', False):
         exec_path = sys._MEIPASS
         file_path = os.path.join(exec_path, file_path)
